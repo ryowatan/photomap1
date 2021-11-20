353b3be9fa51fe0e343c6e91a1e97f6aee7b15e9
ryowatan <boowy.hhtm@outlook.jp>
2021-11-20 13:56:54 +0900

    ポートフォリオを追加

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $('.area_btn').click(function(){
+        $('.area_overlay').show();
+        $('.pref_area').show();
+        var area = $(this).data('area');
+        $('[data-list]').hide();
+        $('[data-list="' + area + '"]').show();
+    });
+
+    $('.area_overlay').click(function(){
+        prefReset();
+    });
+
+   
+
+
+
+
+
+});
